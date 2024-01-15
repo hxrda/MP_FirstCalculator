39b01a750b244bc9f5e4fdae155d611a07de26af
hxrda <henein.roda@gmail.com>
2024-01-15 18:05:58 +0200

    Exercise complete

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Alert, TextInput } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	//States:
+	const [firstNumber, setFirstNumber] = useState(0);
+	const [secondNumber, setSecondNumber] = useState(0);
+	const [result, setResult] = useState();
+
+	//Functions:
+
+	const addNumbers = () => {
+		const resultValue = parseFloat(firstNumber) + parseFloat(secondNumber);
+		setResult(resultValue);
+	};
+
+	const subtractNumbers = () => {
+		const resultValue = parseFloat(firstNumber) - parseFloat(secondNumber);
+		setResult(resultValue);
+	};
+
+	//Rendering:
+	return (
+		<View style={styles.container}>
+			<Text>Result: {result}</Text>
+			<View>
+				<TextInput
+					style={styles.textInputContainer}
+					inputMode="numeric"
+					onChangeText={(firstValue) => setFirstNumber(firstValue)}
+				/>
+				<TextInput
+					style={styles.textInputContainer}
+					inputMode="numeric"
+					onChangeText={(secondValue) => setSecondNumber(secondValue)}
+				/>
+			</View>
+			<View style={styles.buttonContainer}>
+				<Button title="+" onPress={addNumbers} />
+				<View style={{ width: 20 }} />
+				<Button title="-" onPress={subtractNumbers} />
+			</View>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	textInputContainer: {
+		width: 200,
+		borderColor: "gray",
+		borderWidth: 1,
+		marginBottom: 10,
+	},
+	buttonContainer: {
+		flexDirection: "row",
+	},
 });
